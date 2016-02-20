fa2c4b99445af138f817cab72f996ce52da4be73
Robert Roeser <rroeser@netflix.com>
2016-02-19 23:49:26 -0800

    first commit

---
+++ src/main/java/io/reactivesocket/websocket/rxnetty/client/WebSocketClientHandler.java
@@ -0,0 +1,7 @@
+package io.reactivesocket.websocket.rxnetty.client;
+
+/**
+ * Created by rroeser on 2/19/16.
+ */
+public class WebSocketClientHandler {
+}
---
+++ src/main/java/io/reactivesocket/websocket/rxnetty/server/ServerWebSocketDuplexConnection.java
@@ -0,0 +1,7 @@
+package io.reactivesocket.websocket.rxnetty.server;
+
+/**
+ * Created by rroeser on 2/19/16.
+ */
+public class ServerWebSocketDuplexConnection {
+}
