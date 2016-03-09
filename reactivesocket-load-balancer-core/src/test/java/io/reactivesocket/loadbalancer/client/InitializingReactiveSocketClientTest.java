d1210c2e58a9599c6006d712216bca90311184fb
Robert Roeser <rroeser@netflix.com>
2016-03-08 19:05:58 -0800

    more tests

---
+++ reactivesocket-load-balancer-core/src/test/java/io/reactivesocket/loadbalancer/client/InitializingReactiveSocketClientTest.java
@@ -0,0 +1,81 @@
+package io.reactivesocket.loadbalancer.client;
+
+import io.reactivesocket.Payload;
+import io.reactivesocket.ReactiveSocket;
+import io.reactivesocket.internal.rx.EmptySubscription;
+import io.reactivesocket.loadbalancer.ReactiveSocketFactory;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import rx.RxReactiveStreams;
+import rx.observers.TestSubscriber;
+
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by rroeser on 3/8/16.
+ */
+public class InitializingReactiveSocketClientTest {
+    @Test
+    public void testInitNewSocketClient() {
+        ReactiveSocket reactiveSocket = Mockito.mock(ReactiveSocket.class);
+
+        Mockito.when(reactiveSocket.requestResponse(Mockito.any(Payload.class))).thenReturn(new Publisher<Payload>() {
+            @Override
+            public void subscribe(Subscriber<? super Payload> s) {
+                s.onNext(new Payload() {
+                    @Override
+                    public ByteBuffer getData() {
+                        return null;
+                    }
+
+                    @Override
+                    public ByteBuffer getMetadata() {
+                        return null;
+                    }
+                });
+                s.onComplete();
+            }
+        });
+
+        ReactiveSocketFactory reactiveSocketFactory = Mockito.mock(ReactiveSocketFactory.class);
+
+        SocketAddress socketAddress = InetSocketAddress.createUnresolved("localhost", 8080);
+
+        Mockito.when(reactiveSocketFactory.call(socketAddress, 10, TimeUnit.SECONDS)).thenReturn(new Publisher<ReactiveSocket>() {
+            @Override
+            public void subscribe(Subscriber<? super ReactiveSocket> s) {
+                s.onSubscribe(EmptySubscription.INSTANCE);
+                s.onNext(reactiveSocket);
+                s.onComplete();
+            }
+        });
+
+        InitializingReactiveSocketClient initializingReactiveSocketClient
+            = new InitializingReactiveSocketClient(reactiveSocketFactory, socketAddress, 10, TimeUnit.SECONDS, 10, TimeUnit.SECONDS);
+
+        Publisher<Payload> payloadPublisher = initializingReactiveSocketClient.requestResponse(new Payload() {
+            @Override
+            public ByteBuffer getData() {
+                return null;
+            }
+
+            @Override
+            public ByteBuffer getMetadata() {
+                return null;
+            }
+        });
+
+        TestSubscriber testSubscriber = new TestSubscriber();
+        RxReactiveStreams.toObservable(payloadPublisher).subscribe(testSubscriber);
+
+        testSubscriber.awaitTerminalEvent();
+        testSubscriber.assertCompleted();
+
+        Mockito.verify(reactiveSocketFactory, Mockito.times(1)).call(socketAddress, 10, TimeUnit.SECONDS);
+    }
+}
